973aeeb54f444e22fb3961c94f671c3e11ec35e0
Twitchton <Twitchton@gmail.com>
2023-11-21 15:28:55 -0400

    Timeline trigger set
    Timeline trigger completed.

--- Assets/Scripts/TriggerVolume.cs
+++ Assets/Scripts/TriggerVolume.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public PlayableDirector timeline;
 
-    // Update is called once per frame
-    void Update()
+    //function called on entering trigger volume
+    private void OnTriggerEnter(Collider other)
     {
-        
+        timeline.Play();
+        gameObject.SetActive(false);
     }
 }
